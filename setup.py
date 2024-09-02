f54e8caa7e7442e24e4776404b9977c9c642b0c0
abdkhanstd <55620704+abdkhanstd@users.noreply.github.com>
2024-09-02 16:52:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="gpumon",  # New package name
-    version="0.0.6",
+    version="0.0.8",
     packages=find_packages(),
     include_package_data=True,
     description="A simple GPU monitoring tool using GPUtil and psutil",
